c79eff0e658594eb99af0f72d58405e76f7c3767
brandonquinonez <brandonvquinonez23@gmail.com>
2026-06-09 16:52:12 -0700

    created a private _checkResponse method in the Api class

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -4,6 +4,14 @@ class Api {
     this._headers = headers;
   }
 
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+
+    return Promise.reject(`Error: ${res.status}`);
+  }
+
   getAppInfo() {
     //part 4 of project: called getUserInfo in this array below
     return Promise.all([this.getInitialCards(), this.getUserInfo()]);
@@ -12,24 +20,14 @@ class Api {
   getInitialCards() {
     return fetch(`${this._baseUrl}/cards`, {
       headers: this._headers,
-    }).then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      return Promise.reject(`Error: ${res.status}`);
-    });
+    }).then(this._checkResponse);
   }
 
   //part 4: created another method, getUserInfo (different base url)
   getUserInfo() {
     return fetch(`${this._baseUrl}/users/me`, {
       headers: this._headers,
-    }).then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      return Promise.reject(`Error: ${res.status}`);
-    });
+    }).then(this._checkResponse);
   }
 
   // other methods for working with the API
@@ -43,12 +41,7 @@ class Api {
         name,
         about,
       }),
-    }).then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      return Promise.reject(`Error: ${res.status}`);
-    });
+    }).then(this._checkResponse);
   }
 }
 
